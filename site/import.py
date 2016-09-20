9ac9656711b6ac7f204c93da0c62015aa12c5684
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-09-20 13:19:49 +0200

    Fixed import script.

--- site/import.py
+++ site/import.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 
 import os
 import re
@@ -8,6 +8,9 @@ import time
 import urllib2
 import codecs
 import pymysql
+import warnings
+import base64
+import hashlib
 
 from Queue import Queue, Empty as EmptyQueue
 from threading import Thread, Event, active_count as thread_count, Lock
@@ -55,6 +58,13 @@ class Downloader(Worker):
 		"""Download specified URL and store it to default directory."""
 		result = False
 
+		if not Import.get_option('download'):
+			Inserter.add_image((
+					file_name,
+					-1
+				))
+			return True
+
 		# try to download data
 		try:
 			remote_file = urllib2.urlopen(url)
@@ -68,48 +78,150 @@ class Downloader(Worker):
 				local_file.write(data)
 				result = True
 
+			# add image for processing
+			Inserter.add_image((
+					file_name,
+					len(data)
+				))
+
 			self.log('Downloaded: {}'.format(file_name))
 
 		return result
 
 
 class Inserter(Worker):
+	image_queue = Queue()
+	item_galleries = {}
+	item_images = {}
+
+	category_index = {
+			1: 'apartments',
+			3: 'vip',
+			4: 'escort',
+			6: 'private',
+			7: 'massage',
+			8: 'spa',
+			9: 'transgender',
+			10: 'gays',
+			13: 'sado',
+			16: 'porn',
+			18: 'required',
+			19: 'forum',
+			21: 'webcam'
+			}
+	category_map = {}
+
+	location_index = {
+			1: 'tel-aviv',
+			2: 'ramat-gan',
+			3: 'rehovot',
+			4: 'lod',
+			5: 'bat-yam',
+			6: 'petah-tikva',
+			7: 'rishon',
+			8: 'givataim',
+			9: 'holon',
+			10: 'eilat',
+			11: 'ashdod',
+			12: 'ashkelon',
+			13: 'bear-sheva',
+			14: 'dimona',
+			15: 'haifa',
+			16: 'krayot',
+			17: 'akko',
+			18: 'nahariya',
+			19: 'galil',
+			20: 'tiberius',
+			21: 'carmiel',
+			22: 'north-settelment',
+			23: 'hadera',
+			24: 'natanya',
+			25: 'herzeliya',
+			26: 'ramat-hasharon',
+			27: 'rannana',
+			28: 'kefar-saba',
+			29: 'hod-hasharon',
+			30: 'jerusalem',
+			31: 'modiin'
+			}
+
 	def run(self):
 		"""Insert data to database."""
 		# create database connection
 		count = 0
 		connection = pymysql.connect(
-					host='localhost',
-					user='root',
-					password='caracal',
-					db='web_engine'
+				host='localhost',
+				user='root',
+				password='caracal',
+				db='web_engine',
+				charset='utf8'
 				)
+
+		self.remove_tags = re.compile('<.*?>')
+
+		# collect existing data
+		self.log('Connected to database. Collecting existing data...')
+		cursor = connection.cursor()
+
+		sql = 'SELECT `id`, `text_id` FROM `shop_categories`;'
+		cursor.execute(sql)
+		data = cursor.fetchall()
+
+		for category_id, category_text_id in data:
+			if len(category_text_id.strip()) == 0: continue  # only categories with text_id
+			self.category_map[category_text_id] = category_id
+
+		cursor.close()
 
 		# process data
 		self.log('Waiting for data.')
 		while not self._stop_event.is_set():
+			# insert data
 			try:
 				# try getting the data from the main queue
 				data = self._queue.get(True, 1)
+				had_data = True
 
 			except EmptyQueue:
 				# there's still nothing in the queue, wait a bit more
-				time.sleep(2)
+				had_data = False
 
 			else:
 				# download image
-				if self.insert(connection, data):
-					count += 1
+				self.insert(connection, data)
+
+			# insert images
+			try:
+				# get data from the image queue
+				file_name, size = self.image_queue.get(True, 1)
+				had_image = True
+
+			except EmptyQueue:
+				# image queue is empty check if we should terminate or continue
+				had_image = False
+
+			else:
+				# insert image to the database
+				self.insert_image(connection, file_name, size)
+				count += 1
+
+			# wait for data
+			if not had_data and not had_image:
+				if active_count() <= 2:
+					break
+
+				time.sleep(2)
 
 		# clean up
 		connection.close()
+		self.log('Database connection closed!')
 
 	def insert(self, connection, data):
 		"""Insert data into database."""
 		cursor = connection.cursor()
 
 		# create gallery
-		gallery_sql = 'INSERT INTO `gallery_group`(`name_he`, `name_en`) VALUES (?, ?);'
+		gallery_sql = 'INSERT INTO `gallery_groups`(`name_he`, `name_en`) VALUES (%s, %s);'
 		name_he = data['name_he'] if 'name_he' in data else ''
 		name_en = data['name_en'] if 'name_en' in data else ''
 
@@ -117,9 +229,112 @@ class Inserter(Worker):
 		gallery_id = cursor.lastrowid
 
 		# create shop item
+		item_sql = (
+				'INSERT INTO `shop_items`(`uid`, `name_he`, `name_en`, `description_he`, `description_en`, `gallery`) '
+				'VALUES (%s, %s, %s, %s, %s, %s);'
+				)
+		description_he = self.remove_tags.sub('', data['description_he']) if 'description_he' in data else ''
+		description_en = self.remove_tags.sub('', data['description_en']) if 'description_en' in data else ''
+		uid = hashlib.new('sha1', str(data['id'])).hexdigest()
+
+		cursor.execute(item_sql, (uid, name_he, name_en, description_he, description_en, gallery_id))
+		item_id = cursor.lastrowid
+
+		# create phone number property
+		phone_number = data['phone'] if 'phone' in data else ''
+		phone_numbers = phone_number.split('|')
+
+		for index, phone_number in enumerate(phone_numbers, 1):
+			phone_sql = (
+					'INSERT INTO `shop_item_properties`(`item`, `text_id`, `type`, `name_he`, `name_en`, `name_ru`, `name_ar`, `value`) '
+					'VALUES (%s, %s, "text", "Phone number", "Phone number", "Phone number", "Phone number", %s);'
+					)
+			field_name = 'phone{}'.format(index)
+			phone_number = phone_number.strip()
+			field_value = 's:{}:"{}";'.format(len(phone_number), phone_number)
+
+			cursor.execute(phone_sql, (item_id, field_name, field_value))
+
+		# add other properties
+		properties = {
+				'age': 'number',
+				'height': 'number',
+				'breast_size': 'text',
+				'language': 'text',
+				'origin': 'text'
+				}
+
+		for property_name, field_type in properties.items():
+			sql = (
+					'INSERT INTO `shop_item_properties`(`item`, `text_id`, `type`, `name_he`, `name_en`, `name_ru`, `name_ar`, `value`) '
+					'VALUES (%s, %s, %s, %s, %s, %s, %s, %s);'
+					)
+			field_name = property_name.replace('_', ' ').capitalize()
+			field_value = 's:0:"";'
+
+			cursor.execute(sql, (item_id, property_name, field_type, field_name, field_name, field_name, field_name, field_value))
+
+		# assign item to categories
+		location_id = int(data['location']) if 'location' in data else None
+		service_id = int(data['category']) if 'category' in data else None
+
+		# assign location cateogry
+		if location_id is not None and self.location_index.has_key(location_id):
+			location_text_id = self.location_index[location_id]
+
+			if location_text_id in self.category_map:
+				category_id = self.category_map[location_text_id]
+				category_sql = 'INSERT INTO `shop_item_membership`(`category`, `item`) VALUES (%s, %s)'
+				cursor.execute(category_sql, (category_id, item_id))
+
+		# assign service category
+		if service_id is not None and service_id in self.category_index:
+			service_text_id = self.category_index[service_id]
+
+			if service_text_id in self.category_map:
+				category_id = self.category_map[service_text_id]
+				category_sql = 'INSERT INTO `shop_item_membership`(`category`, `item`) VALUES (%s, %s)'
+				cursor.execute(category_sql, (category_id, item_id))
+
+		# store gallery association for easy access later on
+		self.item_galleries[item_id] = gallery_id
+
+		# store image file names associated with this item
+		for image in Import.images_to_download:
+			if image in data:
+				self.item_images[data[image]] = item_id
 
 		# commit transaction
 		cursor.close()
+
+	def insert_image(self, connection, file_name, size):
+		"""Insert image to database."""
+		if file_name not in self.item_images:
+			return  # we don't know to which item this image is associated
+
+		# get the gallery id we need to add image to
+		item_id = self.item_images[file_name]
+		gallery_id = self.item_galleries[item_id]
+
+		# make sure gallery is valid
+		if gallery_id is None:
+			return
+
+		# prepare for insertion
+		cursor = connection.cursor()
+		image_sql = 'INSERT INTO `gallery`(`group`, `size`, `filename`) VALUES (%s, %s, %s);'
+
+		if size == -1:
+			size = os.path.getsize(os.path.abspath(os.path.join(Downloader.DESTINATION, file_name)))
+
+		# insert data
+		cursor.execute(image_sql, (gallery_id, size, file_name))
+		cursor.close()
+
+	@classmethod
+	def add_image(self, data):
+		"""Append image data for insertion later."""
+		self.image_queue.put_nowait(data)
 
 
 class Import:
@@ -128,7 +343,7 @@ class Import:
 	data into final database.
 	"""
 	AD_IMAGE_URL_TEMPLATE = 'http://bsexy.co.il/images/pics/{}'
-	THREAD_COUNT = 20
+	THREAD_COUNT = 1
 
 	stdout_lock = Lock()
 	images_to_download = (
@@ -141,7 +356,8 @@ class Import:
 			'image_6',
 			'image_7',
 			'image_8'
-		)
+			)
+	config = None
 
 	def __init__(self):
 		# create queues for communicating with other threads
@@ -153,48 +369,44 @@ class Import:
 
 		# create regular expression for matching data
 		match_item = re.compile(
-					'^INSERT \[dbo\]\.\[sx_ads\] \([^\)]+\) VALUES \('      # table
-					'(?P<id>\d+),\s*'                                       # id
-					'(?P<category>\d+),\s*'                                 # category id
-					'(?P<location>\d+),\s*'                                 # location id
-					'(?P<sex>\d+),\s*'                                      # sex
-					'(?P<promoted>\d+),\s*'                                 # item is promoted
-					'(?:N\'(?P<name_he>.+?(?=\',\s))\'|NULL),\s*'           # name in hebrew
-					'(?:N\'(?P<name_en>.+?(?=\',\s))\'|NULL),\s*'           # name in english
-					'(?:N\'(?P<description_he>.+?(?=\',\s))\'|NULL),\s*'    # description in hebrew
-					'(?:N\'(?P<description_en>.+?(?=\',\s))\'|NULL),\s*'    # description in english
-					'(?:N\'(?P<phone>.+?(?=\',\s))\'|NULL),\s*'             # description in english
+				'^INSERT \[dbo\]\.\[sx_ads\] \([^\)]+\) VALUES \('             # table
+				'(?P<id>\d+),\s*'                                              # id
+				'(?P<category>\d+),\s*'                                        # category id
+				'(?P<location>\d+),\s*'                                        # location id
+				'(?P<sex>\d+),\s*'                                             # sex
+				'(?P<promoted>\d+),\s*'                                        # item is promoted
+				'(?:N\'(?P<name_he>.{0,100}?(?=\',\s))\'|NULL),\s+'            # name in hebrew
+				'(?:N\'(?P<name_en>.{0,150}?(?=\',\s))\'|NULL),\s+'            # name in english
+				'(?:N\'(?P<description_he>.{0,1000}?(?=\',\s))\'|NULL),\s+'    # description in hebrew
+				'(?:N\'(?P<description_en>.{0,1500}?(?=\',\s))\'|NULL),\s+'    # description in english
+				'(?:N\'(?P<phone>.{0,50}?(?=\',\s))\'|NULL),\s+'               # phone number
 
-					'(?:N\'(?P<icon>.+?(?=\',\s))\'|NULL),\s*'              # icon file
-					'(?:N\'(?P<image_main>.+?(?=\',\s))\'|NULL),\s*'        # main image file
-					'(?:N\'(?P<image_1>.+?(?=\',\s))\'|NULL),\s*'           # first image
-					'(?:N\'(?P<image_1_small>[^\']+)\'|NULL),\s*'           # thumbnail for first image
-					'(?:N\'(?P<image_2>.+?(?=\',\s))\'|NULL),\s*'           # second image
-					'(?:N\'(?P<image_2_small>.+?(?=\',\s))\'|NULL),\s*'     # thumbnail for second image
-					'(?:N\'(?P<image_3>.+?(?=\',\s))\'|NULL),\s*'           # third image
-					'(?:N\'(?P<image_3_small>.+?(?=\',\s))\'|NULL),\s*'     # thumbnail for third image
-					'(?:N\'(?P<image_4>.+?(?=\',\s))\'|NULL),\s*'           # fourth image
-					'(?:N\'(?P<image_4_small>.+?(?=\',\s))\'|NULL),\s*'     # thumbnail for fourth image
+				'(?:N\'(?P<icon>.{0,50}?(?=\',\s))\'|NULL),\s+'                # icon file
+				'(?:N\'(?P<image_main>.{0,255}?(?=\',\s))\'|NULL),\s+'         # main image file
+				'(?:N\'(?P<image_1>.{0,255}?(?=\',\s))\'|NULL),\s+'            # rest of the images
+				'(?:N\'(?P<image_1_small>.{0,255}?(?=\',\s))\'|NULL),\s+'
+				'(?:N\'(?P<image_2>.{0,255}?(?=\',\s))\'|NULL),\s+'
+				'(?:N\'(?P<image_2_small>.{0,255}?(?=\',\s))\'|NULL),\s+'
+				'(?:N\'(?P<image_3>.{0,255}?(?=\',\s))\'|NULL),\s+'
+				'(?:N\'(?P<image_3_small>.{0,255}?(?=\',\s))\'|NULL),\s+'
+				'(?:N\'(?P<image_4>.{0,255}?(?=\',\s))\'|NULL),\s+'
+				'(?:N\'(?P<image_4_small>.{0,255}?(?=\',\s))\'|NULL),\s+'
 
-					'(?:N\'(?P<call_to_action_he>.+?(?=\',\s))\'|NULL),\s*' # call to action text in hebrew
-					'(?:N\'(?P<call_to_action_en>.+?(?=\',\s))\'|NULL),\s*' # call to action text in english
-                                                                            # 'CAST\(N\'(?P<date_from>[^\']+)\' AS \w+\),\s*'                      # from date
-                                                                            # 'CAST\(N\'(?P<date_to>[^\']+)\' AS \w+\),\s*'                        # to date
-					'.+?(?=\d)(?P<status>\d+),\s*'                          # status
+				'(?:N\'(?P<call_to_action_he>.{0,1000}?(?=\',\s))\'|NULL),\s+' # call to action text in hebrew
+				'(?:N\'(?P<call_to_action_en>.{0,1500}?(?=\',\s))\'|NULL),\s+' # call to action text in english
+				'.+?(?=\d)(?P<status>\d+),\s+'                                 # status
 
-					'(?:N\'(?P<image_5>.+?(?=\',\s))\'|NULL),\s*'           # fifth image
-					'(?:N\'(?P<image_5_small>.+?(?=\',\s))\'|NULL),\s*'     # thumbnail for fifth image
-					'(?:N\'(?P<image_6>.+?(?=\',\s))\'|NULL),\s*'           # sixth image
-					'(?:N\'(?P<image_6_small>.+?(?=\',\s))\'|NULL),\s*'     # thumbnail for sixth image
-					'(?:N\'(?P<image_7>.+?(?=\',\s))\'|NULL),\s*'           # seventh image
-					'(?:N\'(?P<image_7_small>.+?(?=\',\s))\'|NULL),\s*'     # thumbnail for seventh image
-					'(?:N\'(?P<image_8>.+?(?=\',\s))\'|NULL),\s*'           # eighth image
-					'(?:N\'(?P<image_8_small>.+?(?=\',\s))\'|NULL),'        # thumbnail for eighth image
+				'(?:N\'(?P<image_5>.{0,255}?(?=\',\s))\'|NULL),\s+'            # fifth image
+				'(?:N\'(?P<image_5_small>.{0,255}?(?=\',\s))\'|NULL),\s+'      # thumbnail for fifth image
+				'(?:N\'(?P<image_6>.{0,255}?(?=\',\s))\'|NULL),\s+'            # sixth image
+				'(?:N\'(?P<image_6_small>.{0,255}?(?=\',\s))\'|NULL),\s+'      # thumbnail for sixth image
+				'(?:N\'(?P<image_7>.{0,255}?(?=\',\s))\'|NULL),\s+'            # seventh image
+				'(?:N\'(?P<image_7_small>.{0,255}?(?=\',\s))\'|NULL),\s+'      # thumbnail for seventh image
+				'(?:N\'(?P<image_8>.{0,255}?(?=\',\s))\'|NULL),\s+'            # eighth image
+				'(?:N\'(?P<image_8_small>.{0,255}?(?=\',\s))\'|NULL),'         # thumbnail for eighth image
 
-                                                                            # '.+?(?=CAST)(?:CAST\(N\'(?P<date_start>[^\']+)\' AS \w+\)|NULL),\s*' # start date
-                                                                            # 'CAST\(N\'(?P<date_end>[^\']+)\' AS \w+\),?\s*'                      # end date
-					'[^\)]*\)',                                             # match the rest of the query
-					re.IGNORECASE | re.MULTILINE | re.UNICODE | re.DOTALL
+				'[^\)]*\)',                                                    # match the rest of the query
+				re.IGNORECASE | re.MULTILINE | re.UNICODE | re.DOTALL
 				)
 
 		# create threads
@@ -211,6 +423,7 @@ class Import:
 		Import.log('Parsing...')
 		for match in match_item.finditer(dump):
 			data = match.groupdict()
+			data = dict(filter(lambda item: item[1] is not None, data.items()))
 
 			# add complete data set for inserter to pick up
 			self._data_queue.put_nowait(data)
@@ -239,8 +452,8 @@ class Import:
 
 	def create_threads(self):
 		"""Create all the worker threads."""
-		# for index in xrange(self.THREAD_COUNT):
-		# 	Downloader(index, self._image_queue, self._stop_event).start()
+		for index in xrange(self.THREAD_COUNT):
+			Downloader(index, self._image_queue, self._stop_event).start()
 
 		Inserter(0, self._data_queue, self._stop_event).start()
 
@@ -251,6 +464,21 @@ class Import:
 		print text
 		cls.stdout_lock.release()
 
+	@classmethod
+	def set_config(cls, config):
+		"""Set configuration."""
+		cls.config = config
+
+	@classmethod
+	def get_option(cls, name):
+		"""Return option name."""
+		assert name in cls.config
+		return cls.config[name]
+
 
 if __name__ == '__main__':
+	Import.set_config({
+		'download': False
+		})
+
 	Import()
