a9e76cca7585e7a89fb36787b3dd2c67cc22c53a
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-11-16 20:41:36 +0100

    Initial import for backend modification.

---
+++ site/modules/bsexy/include/backend.js
@@ -0,0 +1,18 @@
+/**
+ * Backend JavaScript
+ * Bsexy
+ *
+ * Copyright (c) 2016. by Way2CU, http://way2cu.com
+ * Authors: Mladen Mijatov
+ */
+
+var Caracal = Caracal || new Object();
+
+
+Caracal.handle_shop_window_open = function(shop_window) {
+
+};
+
+
+$(function() {
+})
