65095ccb8ebd4c37dab084e6a31b5e4fc100e05b
Mladen Mijatov <meaneye.rcf@gmail.com>
2016-11-20 12:10:05 +0100

    Added custom deployment script.

---
+++ custom_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd deploy
+PID_FILE=/tmp/ansible.pid
+
+if [ -f "hosts.txt" ]; then
+	ansible-playbook -i 185.151.197.11, --ask-pass ./custom_deploy.yml &
+
+	echo $! > $PID_FILE
+	wait $!
+	rm -f $PID_FILE
+	exit $?
+
+else
+	echo 'Missing "hosts" file.'
+fi
